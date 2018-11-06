be1f94243ad81faa1e110e700a489257a05551aa
Matt Putnam <putnam@broadinstitute.org>
2018-11-06 18:00:25 -0500

    option 2: implemented with svg, allows rounded corners

--- src/pages/HexagonDemo.js
+++ src/pages/HexagonDemo.js
@@ -1,5 +1,5 @@
 import _ from 'lodash/fp'
-import { div, h } from 'react-hyperscript-helpers'
+import { div, h, polygon, svg } from 'react-hyperscript-helpers'
 import { AutoSizer } from 'react-virtualized'
 import { pure } from 'recompose'
 import * as Nav from 'src/libs/nav'
@@ -9,10 +9,17 @@ import { Component } from 'src/libs/wrapped-components'
 const root3 = Math.sqrt(3)
 
 
-const HexagonLayout = pure(({ size, margin, backgroundColor, children }) => {
+const HexagonLayout = pure(({ size, margin, backgroundColor, borderRadius=0, children }) => {
   const fullSize = size + margin
   const halfSize = fullSize / 2
   const negMargin = (halfSize - 2 * margin) * root3 / 6
+
+  const y1 = size*root3/6
+  const y2 = size*root3/2
+  const height = size*root3*2/3
+
+  const shortRadAdj = borderRadius/2
+  const longRadAdj = borderRadius*root3/2
 
   return h(AutoSizer, { disableHeight: true }, [
     ({ width }) => {
@@ -21,38 +28,45 @@ const HexagonLayout = pure(({ size, margin, backgroundColor, children }) => {
       const numInSecondRow = numInFirstRow - (width - firstRowWidth < halfSize)
       const totalIn2Rows = numInFirstRow + numInSecondRow
 
-      return div({
+      const hex = (child, index) => div({
         style: {
-          display: 'flex', flexWrap: 'wrap', width,
-          padding: `${negMargin}px 0`
-        }
-      }, children.map((child, index) => div({
-        style: {
+          position: 'relative', width: size, height,
           marginRight: margin,
           marginLeft: (index % totalIn2Rows === numInFirstRow) && halfSize,
           marginTop: -negMargin, marginBottom: -negMargin
         }
       }, [
-        div({
+        svg({
+          xmlns: 'http://www.w3.org/2000/svg',
+          viewBox: `0 0 ${size} ${height}`,
           style: {
-            position: 'relative',
-            width: size, height: 2/3 * root3 * size,
-            transform: 'rotate(-60deg) skewY(30deg)',
-            overflow: 'hidden', visibility: 'hidden'
+            width: size, height
           }
         }, [
-          div({
-            style: {
-              position: 'relative',
-              width: '100%', height: '100%', backgroundColor,
-              transform: 'skewY(-30deg) rotate(60deg)',
-              overflow: 'hidden', visibility: 'visible'
-            }
-          }, [
-            child
-          ])
-        ])
-      ])))
+          polygon({
+            points: `${longRadAdj},${y1+shortRadAdj}
+                     ${size/2},${borderRadius}
+                     ${size-longRadAdj},${y1+shortRadAdj}
+                     ${size-longRadAdj},${y2-shortRadAdj}
+                     ${size/2},${height-borderRadius}
+                     ${longRadAdj},${y2-shortRadAdj}`,
+            fill: backgroundColor,
+            stroke: backgroundColor,
+            strokeWidth: borderRadius*root3,
+            strokeLinejoin: 'round'
+          })
+        ]),
+        child
+      ])
+
+      return div({
+        style: {
+          display: 'flex', flexWrap: 'wrap', width,
+          padding: `${negMargin}px 0`
+        }
+      }, [
+        children.map(hex)
+      ])
     }
   ])
 })
@@ -67,7 +81,7 @@ class HexagonDemo extends Component {
       }
     }, [
       h(HexagonLayout, {
-        size: 200, margin: 10, backgroundColor: 'lightblue'
+        size: 200, margin: 16, borderRadius: 20, backgroundColor: 'lightblue'
       }, _.map(number => div({
         style: {
           position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'
